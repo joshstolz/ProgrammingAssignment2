75e02ba88a97afd00fe44dc7d97ec527d6c826ce
joshstolz <josh.stolz@libd.org>
2020-10-13 15:01:52 -0400

    made cache function

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        invertmatrix <- function(invertedmx) m <<- invertedmx
+        getinvert <- function() m
+        list(set = set, get = get,
+             invertmatrix = invertmatrix,
+             getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinvert()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$invertmatrix(m)
+        m
 }
